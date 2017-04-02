3787da1154229944162af44d04c35e6f309d0858
Alban Nocaj <alban.nocaj@gmail.com>
2017-04-02 18:24:07 +0200

    ClickTheButton Initial Game

--- app/src/main/java/com/sap/clickthebuttons/GameActivity.java
+++ app/src/main/java/com/sap/clickthebuttons/GameActivity.java
@@ -1,13 +1,74 @@
 package com.sap.clickthebuttons;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+    public Button button;
+    public ConstraintLayout playArea;
+    public DisplayMetrics displayMetrics;
+    public Random randomizer;
+    public int clickCountdown = 10;
+    public long startTime;
+    public float elapsedSeconds;
+    public static final String EXTRA_MESSAGE = "com.sap.clickthebuttons.ELAPSED_SECONDS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        button = (Button) findViewById(R.id.button);
+        //playArea = (ConstraintLayout) findViewById(R.id.playArea);
+        displayMetrics = new DisplayMetrics();
+        button.setText(Integer.toString(clickCountdown));
+        button.setVisibility(View.VISIBLE);
+
+        startTime = System.currentTimeMillis();
+
+    }
+
+
+    public void setButtonPositionRamdomly(){
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels - button.getWidth();
+        int height = displayMetrics.heightPixels - 200 - button.getHeight();
+
+        randomizer = new Random();
+
+        //button.setX(randomizer.nextInt(width) + 44 );
+        button.setX(randomizer.nextInt(width));
+        button.setY(randomizer.nextInt(height));
+        button.setText(Integer.toString(clickCountdown));
+
+    }
+
+    public void setToNextPosition(View view){
+
+        clickCountdown = clickCountdown - 1;
+
+
+        if (clickCountdown < 1) {
+            elapsedSeconds = (float) (System.currentTimeMillis() - startTime) / 1000;
+
+            finishGame();
+            return;
+        }
+        setButtonPositionRamdomly();
+
+    }
+
+    public void finishGame(){
+        Intent intent = new Intent(this, TitleActivity.class);
+
+
+        intent.putExtra(EXTRA_MESSAGE, (Float.toString(elapsedSeconds)));
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/sap/clickthebuttons/TitleActivity.java
+++ app/src/main/java/com/sap/clickthebuttons/TitleActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -14,11 +15,21 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(GameActivity.EXTRA_MESSAGE);
+
+        TextView elapsedTimeView = (TextView) findViewById(R.id.textView2);
+        elapsedTimeView.setText(message);
     }
 
     /** Called when the user taps the Start button */
     public void startGame(View view) {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
+
+
     }
+
 }
