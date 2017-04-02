432c89792ead2852e818bd9a76bc22d8820a424a
Alban Nocaj <alban.nocaj@gmail.com>
2017-04-02 15:43:20 +0200

    03_ClickTheButton

--- app/src/main/java/com/sap/clickthebuttons/TitleActivity.java
+++ app/src/main/java/com/sap/clickthebuttons/TitleActivity.java
@@ -1,7 +1,12 @@
 package com.sap.clickthebuttons;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    /** Called when the user taps the Start button */
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
